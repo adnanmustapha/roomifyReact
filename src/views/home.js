0a7398bd80d2f47de6ae3c012eabc9e9594ceecd
Adnan Mustapha <33831938+adnanmustapha@users.noreply.github.com>
2022-10-26 01:08:19 +0100

    Add files via upload

---
+++ src/views/home.js
@@ -0,0 +1,568 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Favorable This Kouprey</title>
+        <meta property="og:title" content="Favorable This Kouprey" />
+      </Helmet>
+      <div className="home-roomies">
+        <img
+          alt="menu2517"
+          src="/playground_assets/menu2517-zny.svg"
+          className="home-menu"
+        />
+        <div className="home-welcometext">
+          <span className="home-text">
+            <span>“There’s no place like home.”</span>
+          </span>
+          <span className="home-text002">
+            <span>Let’s find you that place!</span>
+          </span>
+          <img
+            alt="Vector1259"
+            src="/playground_assets/vector1259-mic.svg"
+            className="home-vector1"
+          />
+        </div>
+        <button className="home-button">
+          <span className="home-text004">
+            <span>Kick off</span>
+          </span>
+        </button>
+        <div className="home-frame1stcard">
+          <img
+            alt="Rectangle12516"
+            src="/playground_assets/rectangle12516-ldrf-300w.png"
+            className="home-rectangle1"
+          />
+          <div className="home-frame1streview">
+            <div className="home-rating">
+              <img
+                alt="emojionestar268"
+                src="/playground_assets/emojionestar268-auev.svg"
+                className="home-emojionestar"
+              />
+              <img
+                alt="emojionestar2710"
+                src="/playground_assets/emojionestar2710-i5hc.svg"
+                className="home-emojionestar01"
+              />
+              <img
+                alt="emojionestar2712"
+                src="/playground_assets/emojionestar2712-ai62r.svg"
+                className="home-emojionestar02"
+              />
+              <img
+                alt="emojionestar2714"
+                src="/playground_assets/emojionestar2714-fphb.svg"
+                className="home-emojionestar03"
+              />
+              <img
+                alt="emojionestar2716"
+                src="/playground_assets/emojionestar2716-fni8.svg"
+                className="home-emojionestar04"
+              />
+            </div>
+            <span className="home-text006">
+              <span>
+                Orci, est porttitor quis eleifend eu ornare. Quis sed nibh sit
+                nec duis justo.
+              </span>
+            </span>
+            <div className="home-img">
+              <img
+                alt="Ellipse1267"
+                src="/playground_assets/ellipse1267-2aw8-200w.png"
+                className="home-ellipse1"
+              />
+              <img
+                alt="tick2722"
+                src="/playground_assets/tick2722-2z1.svg"
+                className="home-tick"
+              />
+            </div>
+          </div>
+          <div className="home-frame2ndreview">
+            <span className="home-text008">
+              <span>Sed dignissim quis tortor vitae, eros donec enim.</span>
+            </span>
+            <img
+              alt="Ellipse22726"
+              src="/playground_assets/ellipse22726-f458c-200w.png"
+              className="home-ellipse2"
+            />
+            <div className="home-rating1">
+              <img
+                alt="emojionestar2728"
+                src="/playground_assets/emojionestar2728-xj7u.svg"
+                className="home-emojionestar05"
+              />
+              <img
+                alt="emojionestar2730"
+                src="/playground_assets/emojionestar2730-ryta.svg"
+                className="home-emojionestar06"
+              />
+              <img
+                alt="emojionestar2732"
+                src="/playground_assets/emojionestar2732-ljzd.svg"
+                className="home-emojionestar07"
+              />
+              <img
+                alt="emojionestar2734"
+                src="/playground_assets/emojionestar2734-h9fe.svg"
+                className="home-emojionestar08"
+              />
+              <img
+                alt="emojionestar2736"
+                src="/playground_assets/emojionestar2736-2uc.svg"
+                className="home-emojionestar09"
+              />
+            </div>
+            <img
+              alt="tick2845"
+              src="/playground_assets/tick2845-98gq.svg"
+              className="home-tick1"
+            />
+          </div>
+        </div>
+        <div className="home-rentrooms">
+          <span className="home-text010">
+            <span>
+              <span>Rent rooms</span>
+              <br></br>
+              <span>and find roommates</span>
+              <br></br>
+              <span>in our verified community</span>
+            </span>
+          </span>
+          <img
+            alt="Vector23452"
+            src="/playground_assets/vector23452-v3b.svg"
+            className="home-vector2"
+          />
+          <div className="home-text017">
+            <span className="home-text018">
+              <span>
+                <span>Whether you’re looking for a place or</span>
+                <br></br>
+                <span>
+                  just someone to split the rent, Roomies has you covered.
+                </span>
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="home-yourplaceyourrules">
+          <div className="home-text023">
+            <span className="home-text024">
+              <span>
+                <span>Roomies helps you share a room</span>
+                <br></br>
+                <span>
+                  or an entire home on your terms. Welcome to the future of
+                  co-living!
+                </span>
+              </span>
+            </span>
+            <img
+              alt="Line13460"
+              src="/playground_assets/line13460-jwkd.svg"
+              className="home-line1"
+            />
+          </div>
+          <span className="home-text029">
+            <span>Your Place, Your Rules</span>
+          </span>
+        </div>
+        <div className="home-rentingterms">
+          <span className="home-text031">
+            <span>
+              <span>
+                Got rooms to rent? Find someone to
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>move in with you. Roomies makes finding cheap</span>
+              <br></br>
+              <span>rooms or putting your house for rent easier</span>
+              <br></br>
+              <span>than ever.</span>
+            </span>
+          </span>
+          <span className="home-text040">
+            <span>Flexible Renting Terms</span>
+          </span>
+        </div>
+        <div className="home-frame2ndimg">
+          <img
+            alt="Rectangle43461"
+            src="/playground_assets/rectangle43461-idfi-400w.png"
+            className="home-rectangle4"
+          />
+          <span className="home-text042">
+            <span>Rent Easier</span>
+          </span>
+        </div>
+        <div className="home-subletsafely">
+          <span className="home-text044">
+            <span>Sublet Safely</span>
+          </span>
+          <span className="home-text046">
+            <span>
+              <span>Trust and transparency are the keys to</span>
+              <br></br>
+              <span>safely find rooms for rent,</span>
+              <br></br>
+              <span>let us help with that!</span>
+            </span>
+          </span>
+        </div>
+        <img
+          alt="IMAGE3rdimg404"
+          src="/playground_assets/image3rdimg404-2qll-400w.png"
+          className="home-i-m-a-g-e3rdimg"
+        />
+        <div className="home-trustedby">
+          <div className="home-topcustomers">
+            <span className="home-text053">
+              <span>
+                <span>Trusted by over 100 customers</span>
+                <br></br>
+                <span>worldwide</span>
+              </span>
+            </span>
+            <div className="home-imgs">
+              <img
+                alt="Ellipse34010"
+                src="/playground_assets/ellipse34010-6pe-200h.png"
+                className="home-ellipse3"
+              />
+              <img
+                alt="Ellipse44011"
+                src="/playground_assets/ellipse44011-qy2d-200h.png"
+                className="home-ellipse4"
+              />
+              <img
+                alt="Ellipse54012"
+                src="/playground_assets/ellipse54012-4u5k-200h.png"
+                className="home-ellipse5"
+              />
+            </div>
+          </div>
+          <div className="home-carousel">
+            <div className="home-comments">
+              <img
+                alt="Vector408"
+                src="/playground_assets/vector408-543n.svg"
+                className="home-vector"
+              />
+              <span className="home-text058">
+                <span>
+                  Tempor tristique urna a ultricies vulputate morbi ornare odio
+                  eleifend. A habitant fringilla tincidunt eu at augue sapien,
+                  purus. Pretium sit porttitor sit in nibh dui nulla. Urna arcu
+                  in velit tellus. Mattis elit non sit commodo, lobortis pretium
+                  rhoncus cursus sollicitudin. Tincidunt at vitae, magna odio.
+                  Viverra cras scelerisque id ultrices. Phasellus nisi volutpat
+                  eget enim suspendisse adipiscing malesuada.
+                </span>
+              </span>
+            </div>
+            <div className="home-carouseldots">
+              <img
+                alt="Ellipse64115"
+                src="/playground_assets/ellipse64115-tvq-200h.png"
+                className="home-ellipse6"
+              />
+              <img
+                alt="Ellipse74116"
+                src="/playground_assets/ellipse74116-bgf9-200h.png"
+                className="home-ellipse7"
+              />
+              <img
+                alt="Ellipse84117"
+                src="/playground_assets/ellipse84117-7pzj-200h.png"
+                className="home-ellipse8"
+              />
+            </div>
+            <span className="home-text060">
+              <span>Devon Lane</span>
+            </span>
+          </div>
+        </div>
+        <div className="home-onlinebooking">
+          <span className="home-text062">
+            <span>Online Booking</span>
+          </span>
+          <span className="home-text064">
+            <span>
+              <span>Easily review booking requests from</span>
+              <br></br>
+              <span>
+                potential roommates. Once you approve
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>
+                their booking, we’ll take your listing off the market.
+              </span>
+            </span>
+          </span>
+        </div>
+        <img
+          alt="IMAGE4thimg4127"
+          src="/playground_assets/image4thimg4127-kdt-400w.png"
+          className="home-i-m-a-g-e4thimg"
+        />
+        <div className="home-privacy">
+          <span className="home-text071">
+            <span>Privacy</span>
+          </span>
+          <span className="home-text073">
+            <span>
+              <span>Keep your contact info private by</span>
+              <br></br>
+              <span>chatting to potential roommates through</span>
+              <br></br>
+              <span>the Roomies app or website</span>
+            </span>
+          </span>
+        </div>
+        <div className="home-securepayment">
+          <span className="home-text080">
+            <span>Secure Payments</span>
+          </span>
+          <span className="home-text082">
+            <span>
+              <span>
+                Once you find a room,
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>
+                we hold and safely transfer your first month’s
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>rent payment to the lister.</span>
+              <br></br>
+              <span>No sketchy, offline cash payments!</span>
+            </span>
+          </span>
+          <img
+            alt="Vector34130"
+            src="/playground_assets/vector34130-sfb.svg"
+            className="home-vector3"
+          />
+        </div>
+        <img
+          alt="IMAGE5thimg4299"
+          src="/playground_assets/image5thimg4299-onff-400w.png"
+          className="home-i-m-a-g-e5thimg"
+        />
+        <div className="home-lookingforroommate">
+          <div className="home-rentaroom">
+            <img
+              alt="Normal4254"
+              src="/playground_assets/normal4254-frso-200h.png"
+              className="home-normal"
+            />
+            <span className="home-text091">
+              <span>Filling an Empty Room?</span>
+            </span>
+            <div className="home-button1">
+              <span className="home-text093">
+                <span>Create a listing</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="home-lookingforroommate1">
+          <div className="home-rentaroom1">
+            <img
+              alt="Normal4264"
+              src="/playground_assets/normal4264-dams-200h.png"
+              className="home-normal1"
+            />
+            <span className="home-text095">
+              <span>Looking for a Room?</span>
+            </span>
+            <div className="home-button2">
+              <span className="home-text097">
+                <span>Search Rooms</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="home-reviews">
+          <span className="home-text099">
+            <span>Roomies Testimonials</span>
+          </span>
+          <div className="home-comment1">
+            <span className="home-text101">
+              <span>
+                Porta aliquam, ut est molestie eu. Mauris, quam volutpat risus
+                etiam rhoncus.
+              </span>
+            </span>
+            <img
+              alt="comments4277"
+              src="/playground_assets/comments4277-sw5w.svg"
+              className="home-comments1"
+            />
+            <img
+              alt="comments4279"
+              src="/playground_assets/comments4279-jtl.svg"
+              className="home-comments2"
+            />
+            <span className="home-text103">
+              <span>-Alex</span>
+            </span>
+          </div>
+          <div className="home-comment2">
+            <span className="home-text105">
+              <span>
+                In hendrerit lectus lacus et nulla mattis proin. Egest ultricies
+                viverra ut morbi leo.
+              </span>
+            </span>
+            <img
+              alt="comments4285"
+              src="/playground_assets/comments4285-iyr.svg"
+              className="home-comments3"
+            />
+            <img
+              alt="comments4287"
+              src="/playground_assets/comments4287-7ig8.svg"
+              className="home-comments4"
+            />
+            <span className="home-text107">
+              <span>-Ralph</span>
+            </span>
+          </div>
+          <div className="home-comment3">
+            <span className="home-text109">
+              <span>
+                Tellus est eget sit sed tempus. Pretium mi ac at massa dolo
+                donec egestas turpis.
+              </span>
+            </span>
+            <img
+              alt="comments4291"
+              src="/playground_assets/comments4291-f4k9.svg"
+              className="home-comments5"
+            />
+            <img
+              alt="comments4293"
+              src="/playground_assets/comments4293-oqn.svg"
+              className="home-comments6"
+            />
+            <span className="home-text111">
+              <span>-Emma</span>
+            </span>
+          </div>
+        </div>
+        <div className="home-footer">
+          <div className="home-contact">
+            <span className="home-text113">
+              <span>Terms &amp; Conditions</span>
+            </span>
+            <span className="home-text115">
+              <span>Help Center</span>
+            </span>
+            <span className="home-text117">
+              <span>Contact Support</span>
+            </span>
+            <span className="home-text119">
+              <span>Contact:</span>
+            </span>
+          </div>
+          <div className="home-media">
+            <span className="home-text121">
+              <span>Testimonials</span>
+            </span>
+            <span className="home-text123">
+              <span>Blog</span>
+            </span>
+            <span className="home-text125">
+              <span>Partnership</span>
+            </span>
+            <span className="home-text127">
+              <span>Media:</span>
+            </span>
+          </div>
+          <div className="home-mission">
+            <span className="home-text129">
+              <span>Our Mission</span>
+            </span>
+            <span className="home-text131">
+              <span>To find you that bubble of comfort!</span>
+            </span>
+          </div>
+          <div className="home-company">
+            <span className="home-text133">
+              <span>How Roomie works</span>
+            </span>
+            <span className="home-text135">
+              <span>About</span>
+            </span>
+            <span className="home-text137">
+              <span>Company:</span>
+            </span>
+          </div>
+          <div className="home-socialmedia">
+            <img
+              alt="linkedinsvgrepocom4519"
+              src="/playground_assets/linkedinsvgrepocom4519-xyd.svg"
+              className="home-linkedinsvgrepocom"
+            />
+            <img
+              alt="instagramsvgrepocom4539"
+              src="/playground_assets/instagramsvgrepocom4539-ptqu.svg"
+              className="home-instagramsvgrepocom"
+            />
+            <img
+              alt="twittersvgrepocom4559"
+              src="/playground_assets/twittersvgrepocom4559-x6xg.svg"
+              className="home-twittersvgrepocom"
+            />
+          </div>
+          <div className="home-copyright">
+            <img
+              alt="copyright4416"
+              src="/playground_assets/copyright4416-f6ha.svg"
+              className="home-copyright1"
+            />
+            <span className="home-text139">
+              <span>2022 Roomies inc.</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
